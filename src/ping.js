fc6514dde0652c225fd997b9f5482fa419857d9c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-03-30 15:07:09 +0700

    Add ping test using k6
    This commit adds a new ping.js test file for load testing with k6. The test file includes configurations for virtual users and the test duration, as well as the test function to send a GET request to the /ping route.

---
+++ src/ping.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  // A number specifying the number of VUs to run concurrently.
+  vus: 10,
+  // A string specifying the total duration of the test run.
+  duration: '30s',
+};
+
+export default function() {
+  http.get('http://localhost:3000/ping');
+  // sleep(1);
+}
