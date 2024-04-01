edadd79169e921cca9e2e8e6a7f3e375839b2779
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-04-01 17:11:25 +0700

    Add src/scenario.js to handle user registration and contact creation
    This commit introduces a new file, src/scenario.js, in which user registration and contact creation functions are defined to simulate user behavior. The number of users and contacts are governed by shared-iterations and constant-vus executor respectively. The success and error of user registration are tracked using 'user_registration_counter_success' and 'user_registration_counter_error'.

---
+++ src/scenario.js
@@ -0,0 +1,60 @@
+import {loginUser, registerUser} from "./helper/user.js";
+import {createContact} from "./helper/contact.js";
+import execution from "k6/execution";
+import {Counter} from "k6/metrics";
+
+export const options = {
+    scenarios: {
+        userRegistration: {
+            exec: "userRegistration",
+            executor: "shared-iterations",
+            vus: 10,
+            iterations: 200,
+            maxDuration: "30s"
+        },
+        contactCreation: {
+            exec: "contactCreation",
+            executor: "constant-vus",
+            vus: 10,
+            duration: "10s"
+        }
+    }
+}
+
+const registerCounterSuccess = new Counter("user_registration_counter_success");
+const registerCounterError = new Counter("user_registration_counter_error");
+
+export function userRegistration() {
+    const uniqueId = new Date().getTime();
+    const registerRequest = {
+        username: `user-${uniqueId}`,
+        password: 'rahasia',
+        name: 'Programmer Zaman Now',
+    };
+
+    const response = registerUser(registerRequest);
+    if (response.status === 200) {
+        registerCounterSuccess.add(1);
+    } else {
+        registerCounterError.add(1);
+    }
+}
+
+export function contactCreation() {
+    const number = (execution.vu.idInInstance % 9) + 1;
+    const username = `contoh${number}`
+    const loginRequest = {
+        username: username,
+        password: 'rahasia',
+    }
+
+    const loginResponse = loginUser(loginRequest);
+    const token = loginResponse.json().data.token;
+
+    const contact = {
+        "first_name": "Kontak",
+        "last_name": `Contoh`,
+        "email": `contact@example.com`,
+    };
+    createContact(token, contact);
+}
