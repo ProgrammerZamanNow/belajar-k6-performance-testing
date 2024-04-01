ae61047e4bbd43863613a1ea24132421564ce9c9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-04-01 15:34:22 +0700

    Add fail conditions to registration and login functionality
    Updated the register.js file to add fail conditions to user registration and login operations. This helps to enhance error visibility when the user registration or login processes fail. It further includes changes to variable naming for better code readability.

--- src/register.js
+++ src/register.js
@@ -1,47 +1,55 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import {fail, sleep} from 'k6';
 
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 10,
-  // A string specifying the total duration of the test run.
-  duration: '10s'
+    // A number specifying the number of VUs to run concurrently.
+    vus: 10,
+    // A string specifying the total duration of the test run.
+    duration: '10s'
 };
 
-export default function() {
-  const uniqueId = new Date().getTime();
-  const body = {
-    username: `user-${uniqueId}`,
-    password: 'rahasia',
-    name: 'Programmer Zaman Now',
-  };
-  http.post('http://localhost:3000/api/users', JSON.stringify(body), {
-    headers: {
-      'Accept' : 'application/json',
-      'Content-Type' : 'application/json',
+export default function () {
+    const uniqueId = new Date().getTime();
+    const registerRequest = {
+        username: `user-${uniqueId}`,
+        password: 'rahasia',
+        name: 'Programmer Zaman Now',
+    };
+    const registerResponse = http.post('http://localhost:3000/api/users', JSON.stringify(registerRequest), {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    })
+    if (registerResponse.status !== 200) {
+        fail(`Failed to register user-${uniqueId}`);
     }
-  })
 
-  const loginBody = {
-    username: `user-${uniqueId}`,
-    password: 'rahasia',
-  }
-
-  const response = http.post('http://localhost:3000/api/users/login', JSON.stringify(loginBody), {
-    headers: {
-      'Accept' : 'application/json',
-      'Content-Type' : 'application/json',
+    const loginRequest = {
+        username: `user-${uniqueId}`,
+        password: 'rahasia',
     }
-  });
 
-  const responseBody = response.json();
-
-  const currentResponse = http.get('http://localhost:3000/api/users/current', {
-    headers: {
-      'Accept' : 'application/json',
-      'Authorization' : responseBody.data.token,
+    const loginResponse = http.post('http://localhost:3000/api/users/login', JSON.stringify(loginRequest), {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    });
+    if (loginResponse.status !== 200) {
+        fail(`Failed to login user-${uniqueId}`);
     }
-  });
 
-  const currentBody = currentResponse.json();
+    const loginBodyResponse = loginResponse.json();
+
+    const currentResponse = http.get('http://localhost:3000/api/users/current', {
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': loginBodyResponse.data.token,
+        }
+    });
+
+    if (currentResponse.status !== 200) {
+        fail(`Failed to get user-${uniqueId}`);
+    }
 }
